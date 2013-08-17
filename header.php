61cbcbf78fc8edb3fb2842176f33b8a5c895b9b5
Sean Winter <drkrift@gmail.com>
2013-08-16 19:04:48 -0700

    Adding some ad code
    ad code script dropped into header.php
    
    ad calls in header and footer .php

--- header.php
+++ header.php
@@ -16,12 +16,65 @@
 <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto:400,700' rel='stylesheet' type='text/css'>
 <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <?php wp_head();?>
+  <script type="text/javascript">
+    //REQUIRED
+    yld_mgr = {};
+    yld_mgr.container_type="js";
+    yld_mgr.request_type="ac";
+    yld_mgr.pub_id="27488010960";
+    yld_mgr.site_name="The Press Democrat HP RE Autos";
+    yld_mgr.content_topic_id_list=["20201001"];
+
+    //SET UP SLOTS
+    yld_mgr.slots = {};
+      //leaderboard 1
+      yld_mgr.slots.ad728x90pos1 = {};
+      yld_mgr.slots.ad728x90pos1.ad_size_list=["728x90"];
+      yld_mgr.slots.ad728x90pos1.ad_delivery_mode="ipatf";
+      yld_mgr.slots.ad728x90pos1.cstm_content_cat_list=[""];
+      //leaderboard 2
+      yld_mgr.slots.ad728x90pos2 = {};
+      yld_mgr.slots.ad728x90pos2.ad_size_list = ["728x90"];
+      yld_mgr.slots.ad728x90pos2.ad_delivery_mode = "ipbtf";
+      yld_mgr.slots.ad728x90pos2.cstm_content_cat_list=[""];
+      //freeform
+      yld_mgr.slots.ad300x600pos1 = {};
+      yld_mgr.slots.ad300x600pos1.ad_size_list = ["300x600"];
+      yld_mgr.slots.ad300x600pos1.ad_delivery_mode = "ipbtf";
+      yld_mgr.slots.ad300x600pos1.cstm_content_cat_list=[""];
+      //large rectangle 1
+      yld_mgr.slots.ad300x250pos1 = {};
+      yld_mgr.slots.ad300x250pos1.ad_size_list = ["300x250"];
+      yld_mgr.slots.ad300x250pos1.ad_delivery_mode = "ipatf";
+      yld_mgr.slots.ad300x250pos1.cstm_content_cat_list=[""];
+      //large rectangle 2
+      yld_mgr.slots.ad300x250pos2 = {};
+      yld_mgr.slots.ad300x250pos2.ad_size_list = ["300x250"];
+      yld_mgr.slots.ad300x250pos2.ad_delivery_mode = "ipbtf";
+      yld_mgr.slots.ad300x250pos2.cstm_content_cat_list=[""];
+      //rich media 2
+      yld_mgr.slots.ad1x1pos1 = {};
+      yld_mgr.slots.ad1x1pos1.ad_size_list = ["1x1"];
+      yld_mgr.slots.ad1x1pos1.ad_delivery_mode = "ipatf";
+      yld_mgr.slots.ad1x1pos1.cstm_content_cat_list=[""];
+      //beltway 1
+      yld_mgr.slots.ad234x60pos1 = {};
+      yld_mgr.slots.ad234x60pos1.ad_size_list = ["234x60"];
+      yld_mgr.slots.ad234x60pos1.ad_delivery_mode = "ipatf";
+      yld_mgr.slots.ad234x60pos1.cstm_content_cat_list=[""] ;
+      //beltway 2
+      yld_mgr.slots.ad234x60pos2 = {};
+      yld_mgr.slots.ad234x60pos2.ad_size_list = ["234x60"];
+      yld_mgr.slots.ad234x60pos2.ad_delivery_mode = "ipbtf";
+      yld_mgr.slots.ad234x60pos2.cstm_content_cat_list=[""] ; 
+    </script>  
+    <script type="text/javascript" src="http://e.yieldmanager.net/script.js"></script>
 </head>
 <body>
 <div class="wrapper">
 <header role="banner">
-	<div class="ad">
-		<img src="http://placekitten.com/728/90" width="728" height="90" />
+	<div id="leader_pos1" class="ad ad-leader">
+		<script>yld_mgr.place_ad_here("ad728x90pos1");</script>
 	</div>
 	<nav class="top-nav">
 		<?php echo strip_tags(wp_nav_menu(array('theme_location' => 'top-menu', 'items_wrap' => '%3$s', 'container' => '','echo' => false, 'depth' => 0) ), '<a>'); ?>
